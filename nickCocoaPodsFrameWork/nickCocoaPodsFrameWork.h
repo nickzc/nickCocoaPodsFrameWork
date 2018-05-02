ca1d5a87921ff2d0e323c9a270d76a4a82f61ac7
有人zc <654306209@qq.com>
2018-05-02 22:32:09 +0800

    创建项目

---
+++ nickCocoaPodsFrameWork/nickCocoaPodsFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  nickCocoaPodsFrameWork.h
+//  nickCocoaPodsFrameWork
+//
+//  Created by JHONSON on 2018/5/2.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nickCocoaPodsFrameWork.
+FOUNDATION_EXPORT double nickCocoaPodsFrameWorkVersionNumber;
+
+//! Project version string for nickCocoaPodsFrameWork.
+FOUNDATION_EXPORT const unsigned char nickCocoaPodsFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nickCocoaPodsFrameWork/PublicHeader.h>
+
+
